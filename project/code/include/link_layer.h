e2cc72706ae074ca1ee7cccfb812e41681cb3590
Rodrigo Martins <rodrigomartins8828@gmail.com>
2024-10-09 14:59:57 +0100

    make defines

--- project/code/include/link_layer.h
+++ project/code/include/link_layer.h
@@ -26,6 +26,15 @@ typedef struct
 // MISC
 #define FALSE 0
 #define TRUE 1
+#define FLAG 0x7E
+#define A_T 0x03
+#define A_R 0x01
+#define C_SET 0X03
+#define C_UA 0X07
+#define C_RR0 0xAA
+#define C_RR1 0xAB
+#define C_REJ0 0x54
+#define C_DISC 0x0B
 
 // Open a connection using the "port" parameters defined in struct linkLayer.
 // Return "1" on success or "-1" on error.
