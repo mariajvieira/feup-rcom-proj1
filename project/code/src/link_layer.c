34696c57d9e54f082e8cfbb85603fa7ca14cc163
Rodrigo Martins <rodrigomartins8828@gmail.com>
2024-10-15 18:34:17 +0100

    case Tx done (not compiled)

--- project/code/src/link_layer.c
+++ project/code/src/link_layer.c
@@ -2,7 +2,7 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
-#define BUF_SIZE 5 
+#define BUF_SIZE 5
 #define FLAG 0x7E
 #define A_T 0x03
 #define A_R 0x01
@@ -16,6 +16,8 @@
 int alarmcount = 0;
 int alarmEnabled = FALSE;
 volatile int STOP = FALSE;
+int ret, timeout;
+int fd;
 
 void alarmHandler(int signal)
 {
@@ -33,60 +35,138 @@ void alarmHandler(int signal)
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
-    if (openSerialPort(connectionParameters.serialPort,
-                       connectionParameters.baudRate) < 0)
+    fd = openSerialPort(connectionParameters.serialPort,
+                       connectionParameters.baudRate);
+    
+    if (fd < 0 ){
+        perror(connectionParameters.serialPort);
+        exit(-1);
+    }
+    timeout = connectionParameters.timeout;
+    ret = connectionParameters.nRetransmissions;
+
+    printf("Serial port open");
+
+switch (connectionParameters.role){
+    
+    case(LlTx):
     {
-        return -1;
+        (void) signal (SIGALRM, alarmHandler); //set alarm function handler
+
+        unsigned char buf [BUF_SIZE] = {0}; 
+        unsigned char pass [BUF_SIZE] = {0};
+        unsigned char receive[BUF_SIZE] = {0};
+
+        buf[0] = 0x7E;
+        buf[1] = 0x03;
+        buf[2] = 0x03;
+        buf[3] = (buf[1])^(buf[2]);
+        buf[4] = 0x7E;
+        buf[5] = '\n';
+
+        int count = 0 ; 
+        int flag = 0;
+        int final = 0;
+
+        while (alarmcount < ret) {
+            if (!alarmEnabled){
+                int bytesW = writeBytesSerialPort (buf, 5); //serial port
+                printf("%d bytes written\n", bytesW);
+                alarm(timeout);            
+                alarmEnabled = TRUE;
+            }
+            while (alarmEnabled && !STOP){
+                int bytesR = readByteSerialPort (pass); //serial port
+                if (bytesR == 0) continue ; //if no byte was received, continue
+
+                if (receive[0] == 0) { //if receive[0] = 0 , new message 
+                    count = 0; 
+                }
+
+                if (flag) { // reception error , will start again
+                    count = 1;
+                    receive [0] = 0x7E; // start flag
+                    flag = 0 ;
+                }
+
+                receive [count] = pass[0]; // read buffer (.79) to receive buffer
+
+                printf("buf[%d] = 0x%02X\n", count, (unsigned int)(receive[count]));
+
+                switch (count) {
+                    case 0 : 
+                        if (receive[count] != 0x7E){
+                            receive[0] = 0;
+                        }
+                        break;
+                    
+                    case 1: 
+                        if (receive[count] != 0x03){
+                            if (receive[count] == 0x7E){
+                                flag = 1;
+                            }
+                            else{
+                                receive[0] = 0;
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        if (receive[count] != 0x07){
+                            if (receive[count] == 0x07){
+                                flag = 1;
+                            }
+                            else {
+                                receive[0] = 0;
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        if (receive[count] != (receive[1] ^ receive [2])){
+                            if (receive[count] == 0x7E) {
+                                flag = 1;
+                            }
+                            else{
+                                receive[0] = 0;
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        if (receive[count] == 0x7E){
+                            STOP = TRUE;
+                            final = 1;
+                        }
+                        else{
+                            if (receive[count] = 0x7E) {
+                                flag = 1;
+                            }
+                            else{
+                                receive[0] = 0;
+                            }
+                        }
+                        break;
+                    
+                    default:
+                        break;
+                    
+                }
+                count++;
+            }
+
+            if (final){
+                break;
+            }
+
+        }
+        if (STOP) return fd ; 
+        
     }
 
-   unsigned char buf[BUF_SIZE] = {0};
+}
 
-   buf[0] = 0x7E;
-   buf[1] = 0x03;
-   buf[2] = 0x03;
-   buf[3] = (buf[1])^(buf[2]);
-   buf[4] = 0x7E;
 
-   (void)signal(SIGALRM, alarmHandler);
-
-   int bytes = writeBytesSerialPort(buf, 5);
-   alarm(3);
-   alarmEnabled = TRUE;
-   printf("%d bytes written\n", bytes);
-
-   unsigned char bufr[BUF_SIZE] = {0};
-
-   
-   while (STOP == FALSE && alarmcount<4)
-   {
-       // Returns after 5 chars have been input
-       bytes = readByteSerialPort(bufr);
-       if (bytes>0) {
-           alarmEnabled=FALSE;
-           printf("Received :)\n");
-
-                   
-           if (buf[0]==0X7E && buf[4]==0X7E && buf[2]==0X07 && (buf[3]==(buf[1]^buf[2]))) {
-               
-               STOP = TRUE;
-           
-               printf("UA received\n");
-           }
-
-           STOP=TRUE;
-
-       } else if (alarmEnabled==FALSE && alarmcount<4) {
-
-           writeBytesSerialPort(buf, 5);
-           alarm(3);
-           alarmEnabled=TRUE;
-       }
-
-   }
-   if (!STOP) {
-       printf("Error :(");
-       exit(1);
-   }
 
     return 1;
 }
