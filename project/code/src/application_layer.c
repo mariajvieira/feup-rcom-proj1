acc4d561af21060377aee0eabe3a5ecdeb6a1b38
mariajvieira <up202204802@up.pt>
2024-10-28 14:27:51 +0000

    start application layer

--- project/code/src/application_layer.c
+++ project/code/src/application_layer.c
@@ -27,5 +27,30 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
 
     llopen(test);
     printf("CLOSING...\n");
+
+    switch(test.role) {
+        case (LlTx):
+        {
+
+
+            llclose(0);
+            break;
+        }
+        case (LlRx):
+        {
+
+
+
+            llclose(0);
+            break;
+        }
+
+
+    }
+
+
+
+
+
     llclose(1);
 }
